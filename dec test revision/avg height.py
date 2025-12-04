d14fd523c03d0afc85ace072ef9c87785ce108a9
JesthanCystl <won108@stge.org.uk>
2025-12-04 12:02:38 +0000

    test

--- dec test revision/avg height.py
+++ dec test revision/avg height.py
@@ -1,15 +1,9 @@
 totalHeight = 0
 counter = 0
-for i in range(6):
+for i in range(2):
     height = input("height?")
     totalHeight = totalHeight + int(height)
     counter = counter + 1 
 print("Average is : ", totalHeight/counter)
 
 
-counta = 0
-while counta < 3:
-    word = input("input a word: ")
-    counta = 0 
-
-    for letter in 
---
+++ test/children_score.py
@@ -0,0 +1,6 @@
+import random 
+
+myRandomNumber =  random.randint(1,100)
+for i in range(10) : 
+    if random < 50 : 
+        print  child 
