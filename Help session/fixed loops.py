8755cfb22235bb6a1ffc02100c9d2b1e95f52e4d
JesthanCystl <won108@stge.org.uk>
2025-12-09 16:37:00 +0000

    first help ses

---
+++ Help session/fixed loops.py
@@ -0,0 +1,31 @@
+# fixed loop 
+# for hi in range (1100):
+#     print("hello world")
+#     print(hi)
+
+# for counter in range(10,20):
+#     print(counter)
+
+# question 1 :))))))))
+# for i in range (10) :
+#     print("jessie")
+
+# question 2 :))))))))
+# name = input("what is your name: ")
+
+# for i in range (1000) :
+#     print(name)
+
+# q3
+# name = input("what is your name: ")
+
+# for i in range (1000) :
+#     print("hello " + name)
+
+# q4
+
+for i in range (1,10000):
+    print ("8 x " + str(i) + " = " + str(i*8))
+
+
+
---
+++ Help session/intro to coding.py
@@ -0,0 +1,7 @@
+#  data types 
+name = "jessie wong" #string
+age = 15 #integer = whole number
+age_in_five_years = age+5
+ageInSevenYears =  age+7
+# print(ageInSevenYears)
+
